c1fae17ea3d8b13c67367f7aa21b13d8e35021c1
Nicholas R Johnson <njohnson@nicholasrjohnson.com>
2021-09-26 08:53:39 -0400

    Made white background:

--- Program.cs
+++ Program.cs
@@ -1,116 +1,122 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace nicksite
 {
     public class Program
-{
-    public static void Main(string[] args)
     {
-        BuildWebHost(args).Run();
-    }
-
-    public static IWebHost BuildWebHost(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .UseKestrel(options => options.ConfigureEndpoints())
-            .Build();
-}
-
-public static class KestrelServerOptionsExtensions
-{
-    public static void ConfigureEndpoints(this KestrelServerOptions options)
-    {
-        var configuration = options.ApplicationServices.GetRequiredService<IConfiguration>();
-        var environment = options.ApplicationServices.GetRequiredService<IHostingEnvironment>();
-
-        var endpoints = configuration.GetSection("HttpServer:Endpoints")
-            .GetChildren()
-            .ToDictionary(section => section.Key, section =>
-            {
-                var endpoint = new EndpointConfiguration(); 
-                section.Bind(endpoint); 
-                return endpoint;
-            });
-
-        foreach (var endpoint in endpoints)
+        public static void Main(string[] args)
         {
-            var config = endpoint.Value;
-            var port = config.Port ?? (config.Scheme == "https" ? 443 : 80);
-
-            var ipAddresses = new List<IPAddress>();
-            if (config.Host == "localhost")
-            {
-                ipAddresses.Add(IPAddress.IPv6Loopback);
-                ipAddresses.Add(IPAddress.Loopback);
-            }
-            else if (IPAddress.TryParse(config.Host, out var address))
-            {
-                ipAddresses.Add(address);
-            }
-            else
-            {
-                ipAddresses.Add(IPAddress.IPv6Any);
-            }
-
-            foreach (var address in ipAddresses)
-            {
-                options.Listen(address, port,
-                    listenOptions =>
-                    {
-                        if (config.Scheme == "https")
-                        {
-                            var certificate = LoadCertificate(config, environment);
-                            listenOptions.UseHttps(certificate);
-                        }
-                    });
-            }
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseKestrel(options => options.ConfigureEndpoints())
+                .Build();
     }
 
-    private static X509Certificate2 LoadCertificate(EndpointConfiguration config, IHostingEnvironment environment)
+    public static class KestrelServerOptionsExtensions
     {
-        if (config.StoreName != null && config.StoreLocation != null)
+        public static void ConfigureEndpoints(this KestrelServerOptions options)
         {
-            using (var store = new X509Store(config.StoreName, Enum.Parse<StoreLocation>(config.StoreLocation)))
-            {
-                store.Open(OpenFlags.ReadOnly);
-                var certificate = store.Certificates.Find(
-                    X509FindType.FindBySubjectName,
-                    config.Host,
-                    validOnly: !environment.IsDevelopment());
+            var configuration = options.ApplicationServices.GetRequiredService<IConfiguration>();
+            var environment = options.ApplicationServices.GetRequiredService<Microsoft.Extensions.Hosting.IHostEnvironment>();
 
-                if (certificate.Count == 0)
+            var endpoints = configuration.GetSection("HttpServer:Endpoints")
+                .GetChildren()
+                .ToDictionary(section => section.Key, section =>
                 {
-                    throw new InvalidOperationException($"Certificate not found for {config.Host}.");
+                    var endpoint = new EndpointConfiguration(); 
+                    section.Bind(endpoint); 
+                    return endpoint;
+                });
+
+            foreach (var endpoint in endpoints)
+            {
+                var config = endpoint.Value;
+                var port = config.Port ?? (config.Scheme == "https" ? 443 : 80);
+
+                var ipAddresses = new List<IPAddress>();
+                if (config.Host == "localhost")
+                {
+                    ipAddresses.Add(IPAddress.IPv6Loopback);
+                    ipAddresses.Add(IPAddress.Loopback);
+                }
+                else if (IPAddress.TryParse(config.Host, out var address))
+                {
+                    ipAddresses.Add(address);
+                }
+                else
+                {
+                    ipAddresses.Add(IPAddress.IPv6Any);
                 }
 
-                return certificate[0];
+                foreach (var address in ipAddresses)
+                {
+                    options.Listen(address, port,
+                        listenOptions =>
+                        {
+                            if (config.Scheme == "https")
+                            {
+                                var certificate = LoadCertificate(config, environment);
+                                listenOptions.UseHttps(certificate);
+                            }
+                        });
+                }
             }
         }
 
-        if (config.FilePath != null && config.Password != null)
+        private static X509Certificate2 LoadCertificate(EndpointConfiguration config, Microsoft.Extensions.Hosting.IHostEnvironment environment)
         {
-            return new X509Certificate2(config.FilePath, config.Password);
+            if (config.StoreName != null && config.StoreLocation != null)
+            {
+                using (var store = new X509Store(config.StoreName, Enum.Parse<StoreLocation>(config.StoreLocation)))
+                {
+                    store.Open(OpenFlags.ReadOnly);
+                    var certificate = store.Certificates.Find(
+                        X509FindType.FindBySubjectName,
+                        config.Host,
+                        validOnly: !environment.IsDevelopment());
+
+                    if (certificate.Count == 0)
+                    {
+                        throw new InvalidOperationException($"Certificate not found for {config.Host}.");
+                    }
+
+                    return certificate[0];
+                }
+            }
+
+            if (config.FilePath != null && config.Password != null)
+            {
+                return new X509Certificate2(config.FilePath, config.Password);
+            }
+
+            throw new InvalidOperationException("No valid certificate configuration found for the current endpoint.");
         }
-
-        throw new InvalidOperationException("No valid certificate configuration found for the current endpoint.");
     }
-}
 
-public class EndpointConfiguration
-{
-    public string Host { get; set; }
-    public int? Port { get; set; }
-    public string Scheme { get; set; }
-    public string StoreName { get; set; }
-    public string StoreLocation { get; set; }
-    public string FilePath { get; set; }
-    public string Password { get; set; }
+    public class EndpointConfiguration
+    {
+        public string Host { get; set; }
+        public int? Port { get; set; }
+        public string Scheme { get; set; }
+        public string StoreName { get; set; }
+        public string StoreLocation { get; set; }
+        public string FilePath { get; set; }
+        public string Password { get; set; }
+    }
 }
